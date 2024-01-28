cc0257947bc29b65a3ea83f6490b7c3a71a1d9b9
Josh Sanders <Josh.Sanders@mosaicco.com>
2024-01-28 12:49:58 -0500

    httpclientfactory

--- SaphyreDemo/Program.cs
+++ SaphyreDemo/Program.cs
@@ -12,6 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpClient();
 
 builder.Services.AddMudServices();
 builder.Services.AddScoped<ModalService>();
